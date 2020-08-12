a0ae2688105398089b953e8226439b28b187503f
Ema Delgado <emaaadelgado@gmail.com>
2020-08-12 19:11:14 -0300

    sidebarV1.4

--- src/components/sidebar/Sidebar.js
+++ src/components/sidebar/Sidebar.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import logo from '../sidebar/logo.png';
 // import items from '../sidebar/items';
 // import 'materialize-css/dist/css/materialize.min.css';
 
@@ -12,21 +13,23 @@ class Sidebar extends React.Component {
     render() {
         return (
             <div className="grilla">
+
                 <div className="sidebar" >
+                    <img src="https://i.ibb.co/dbm3s4r/logo.png" alt="logo" className="logo" border="0" />
+                    {/* <img src={logo} alt="logo pagina" className="logo" /> */}
                     <ul className="items">
                         <li><a href={'/'} className="waves-effect waves-light btn-small" onClick={this.foo}>
-                            <i class="material-icons left">{"api"}</i>{"Caja"}</a></li>
+                            <i class="material-icons left">{"shopping_cart"}</i>{"Caja"}</a></li>
                         <li><a href={'/'} className="waves-effect waves-light btn-small" onClick={this.foo}>
                             <i class="material-icons left">{"face"}</i>{"Clientes"}</a></li>
                         <li><a href={'/'} className="waves-effect waves-light btn-small" onClick={this.foo}>
-                            <i class="material-icons left">{"cloud"}</i>{"Proveedores"}</a></li>
-                        <li><a href={'/'} className="waves-effect waves-light btn-small">
+                            <i class="material-icons left">{"local_shipping"}</i>{"Proveedores"}</a></li>
+                        <li><a href={'/'} className="waves-effect waves-light btn-small" onClick={this.foo}>
                             <i class="material-icons left">{"build_circle"}</i>{"Reparaciones"}</a></li>
                     </ul>
                 </div>
                 <div >
                     <nav className="navbar">
-
                         <a href={"/"} data-target="slide-out"
                             className="sidenav-trigger show-on-large " onClick={this.foo}>
                             <i class="material-icons">{"menu"}</i>
